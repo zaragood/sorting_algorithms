65756b38dbfeb784a042c89748e6cd632a35b98c
zaragood <azaragoodness52@gmail.com>
2023-09-29 21:12:09 +0000

    sort.h

--- sort.h
+++ sort.h
@@ -27,7 +27,8 @@ void bubble_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 void swap_ints(int *a, int *b);
-int lomuto_partition(int *array, size_t size, int left, int right);void lomuto_sort(int *array, size_t size, int left, int right);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
@@ -36,5 +37,4 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-
 #endif /*SORT_H*/
